18f3cece7b06e312547e3ac281e55ed6f7bce516
eloureir <ericloureirosud@gmail.com>
2025-11-11 17:06:42 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Form } from './Form';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Header } from './Header';
+import { Home } from './Home';
+import { Consult } from './Consult';
+
+function App() {
+ 
+  return (
+
+    <BrowserRouter>
+    <Header/>
+    <div className="flex items-center justify-center min-h-screen bg-sky-300">
+      <div>
+        <div className='bg-white/50 rounded-lg'> 
+        <main className='p-4'>
+          <Routes>
+            <Route path='/' element={<Home/>} />
+            <Route path='/consultar' element={<Consult/>} />
+            <Route path='/cadastrar' element={<Form/>} />
+          </Routes>
+        </main>
+        </div>
+      </div>
+    </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/Form.js
@@ -0,0 +1,92 @@
+import { useForm } from 'react-hook-form';
+import React, { useState } from "react";
+
+export function Form() {
+
+    const [mensagem, setMensagem] = useState("");
+
+    const { register,
+        handleSubmit,
+        formState: { errors },
+        reset } = useForm({ mode: 'onBlur' });
+    //Arrow Function
+    const onSubmit = async (data) => {
+
+        try {
+            const response = await fetch('http://localhost:8080/api/cadastro',
+                {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(data),
+                }
+            );
+            if (response.ok) {
+                const mensagemSucesso = await response.text();
+                setMensagem({ type: 'sucesso', text: mensagemSucesso })
+                console.log("Resposta do backend", mensagemSucesso);
+                reset()
+            } else {
+                const mensagemFalhou = await response.text();
+                setMensagem({ type: 'falhou', text: mensagemFalhou })
+                console.error('Erro ao cadastrar.');
+
+            }
+        } catch (error) {
+            console.error('Erro ao buscar a mensagem:', error);
+            setMensagem({type: 'falhou', text: 'Erro ao buscar a mensagem no servidor.'});
+
+        }
+    }
+
+    return (
+        <div>
+            <h1 className='text-3xl text-900 mb-4'>Cadastrar Usuários</h1>
+            <form className='space-y-2' onSubmit={handleSubmit(onSubmit)} noValidate>
+                <label className='block text-sm font-medium text-gray-700'>Nome </label>
+                <input
+                    placeholder='Digite seu nome'
+                    {...register("nome", { required: "O nome é obrigatório" })}
+                    className="w-full border rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
+                />
+                {errors.nome && <p style={{ color: "red" }}>{errors.nome.message}</p>}
+                <br />
+                <label className='block text-sm font-medium text-gray-700'>Email </label>
+                <input placeholder='Digite seu email' type='email'
+                    {...register("email", { required: "O email é obrigatório", pattern: { value: /^[^\s@]+@[^\s]+\.[^\s]+$/, message: "Email inválido" } })}
+                    className="w-full border rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
+                />
+                {errors.email && <p style={{ color: "red" }}>{errors.email.message}</p>}<br />
+                <label className='block text-sm font-medium text-gray-700'>Senha </label>
+                <input placeholder='Digite sua senha' type='password'
+                    {...register("senha", { required: "A senha é obrigatória", minLength: { value: 6, message: "A senha deve ter no mínimo 6 caracteres" } })}
+                    className="w-full border rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
+                /><br />
+                {errors.senha && <p style={{ color: "red" }}>{errors.senha.message}</p>}
+                <label className='block text-sm font-medium text-gray-700'>Telefone </label>
+                <input placeholder='(XX)99999999' type='tel' //criar uma máscara, ajustar o placeholder
+                    {...register("telefone", { required: "O telefone é obrigatório", pattern: { value: /^\(\d{2}\)\d{9}$/, message: "Telefone inválido" } })}
+                    className="w-full border rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
+                /><br />
+                {errors.telefone && <p style={{ color: "red" }}>{errors.telefone.message}</p>}
+                <label >Data de Nascimento </label>
+                <input type='date'
+                    {...register("dataNascimento", { required: "A data de nascimento é obrigatória" })}
+                    className="w-full border rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
+                /><br />
+                {errors.dataNascimento && <p style={{ color: "red" }}>{errors.dataNascimento.message}</p>}
+                <div className='flex justify-center'>
+                    <button className="w-1/2 mt-2 p-3 text-white rounded-lg bg-sky-500 hover:bg-sky-700" type='submit'>Enviar</button>
+                </div>
+            </form>
+
+            {mensagem && (
+                <div className={`p-3 rounded-lg text-center ${mensagem.type === 'sucesso' ? 'bg-green-100 text-green-800' : 'bg-red-100 text-red-800'
+                    }`}>{mensagem.text}
+                </div>
+            )}
+
+        </div>
+    )
+}
